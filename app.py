2bbc3e9fa0ed9e66628f3e2f058b88c2da454151
mlee111 <michelle_lee1@intuit.com>
2022-09-07 03:00:49 +0000

    add new tab

--- app.py
+++ app.py
@@ -31,7 +31,8 @@ app.layout = html.Div(children=[
         html.Div([
             dcc.Tabs(id='tabs', value='tab1', children=[
                 dcc.Tab(label='Analysis 1', value='tab1'),
-                dcc.Tab(label='Analysis 2', value='tab2')
+                dcc.Tab(label='Analysis 2', value='tab2'),
+                dcc.Tab(label='Analysis 3', value='tab3')
             ]),
             html.Div(id='tab_content'),
     ]),
@@ -49,6 +50,8 @@ app.layout = html.Div(children=[
 def make_figure(varname):
     mygraphtitle = f'Pokemon grouped by generation'
     mycolorbartitle = "Num Pokemon"
+    
+    colors_dict = {'Grass':'#7deb34', 'Fire':'#eb4f3b', 'Water':'#2983e3', 'Bug':'#3b8c3f', 'Normal':'#d9d3a7', 'Poison':'#b348d4', 'Electric':'#feffb8', 'Ground':'#7a4917','Fairy':'#f7b7e9', 'Fighting':'#4d6ec9', 'Psychic':'#e3a3d6', 'Rock':'#6e6b6d', 'Ghost':'#311559', 'Ice':'#8af1ff', 'Dragon':'#9e233e', 'Dark':'#242323', 'Steel':'#5e5e5e', 'Flying':'#7a93ff'}
 
     subset = df[df['Type 1'] == varname]
     grouped_data = subset.groupby('Generation')
@@ -57,7 +60,8 @@ def make_figure(varname):
     barchart = go.Bar(
         x=df2.index,
         y=df2['Type 1'],
-        name=varname
+        name=varname,
+        marker=dict(color=colors_dict[varname])
     )
     
     chart_layout = go.Layout(
@@ -112,17 +116,33 @@ def render_tabs(tab):
             hoverinfo='text',
         )
         layout2 = go.Layout(
-            title='Pokemon Atk vs Def [Gen 1-3]',
+            title='Pokemon Atk vs Def [Gen 4-6]',
             xaxis_title='Attack',
             yaxis_title='Defense',
 
         )
         last_gen_scatter = go.Figure(data=scatter2, layout=layout2);
         return html.Div([
+            html.H4('Attack and Defense stats (hover to view Pokemon name)'),
             html.H6('Attack + Defense Stats Gen 1-3'),
             html.Div([dcc.Graph(id='figure-2', figure=first_gen_scatter)]),
-            html.H6('Attack + Defense Stats Gen 1-3'),
+            html.H6('Attack + Defense Stats Gen 4-6'),
             html.Div([dcc.Graph(id='figure-3', figure=last_gen_scatter)]),
+        ])
+    elif tab == 'tab3':
+        legendaries = df[df['Legendary'] == True]
+        grouped_data = legendaries.groupby('Generation')
+        totals = pd.DataFrame(grouped_data.mean()['Attack'])
+
+        barchart = go.Bar(
+            x = totals.index,
+            y = totals['Attack']
+        )
+
+        fig3 = go.Figure(data=barchart)
+        return html.Div([
+            html.H4('Average Attack of Legendaries by generation'),
+            html.Div([dcc.Graph(id='figure-3', figure=fig3)]),
         ])
         
     
